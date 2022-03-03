9d6b2c2e17a16a41cd6fbd96ec73bf785e3b36b4
Konteh <kontehebrima993@gmail.com>
2022-03-02 23:05:31 -0800

    newletter section reacted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Announcement from '../components/Announcement'
 import Categories from '../components/Catergories'
 import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
 import Products from '../components/Products'
 import Slider from '../components/Slider'
 
@@ -15,6 +16,7 @@ const Home = () => {
       <Slider></Slider>
       <Categories></Categories>
       <Products></Products>
+      <Newsletter></Newsletter>
     </div>
   )
 }
