31a5ae5ee1462b19f7a8517d9402275ca5260bda
Konteh <kontehebrima993@gmail.com>
2022-03-02 23:43:38 -0800

    single product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-
-import ProductList from "./pages/ProductList";
-
+import Product from "./pages/Product";
 const App = () => {
-  return <ProductList></ProductList>
-
+  return <Product></Product>
 };
 
 export default App;
